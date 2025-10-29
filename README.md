bc4122e577c3b2cbcda2fe722dbe3219600c7f74
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2025-10-30 00:17:48 +0700

    Update README.md

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ sbt new deadblackclover/scala.g8
 ```sh
 sbt fastLinkJS 
 ```
+or
+```sh
+docker run -it -v .:/root/project -w "/root/project" --rm sbtscala/scala-sbt:eclipse-temurin-17.0.2_1.6.2_2.12.15 sbt fastLinkJS
+```
--- src/main/scala/cipherdogs/Hello.scala
+++ src/main/scala/cipherdogs/Hello.scala
@@ -5,7 +5,7 @@ import org.scalajs.dom.document
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
+    appendPar(document.body, "Hello World from Scala.js")
   }
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
